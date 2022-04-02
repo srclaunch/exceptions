5bddf195e430c95e000357d42eb67b7f1d0f3d03
Steven Bennett <steven@budgetbloom.com>
2022-04-02 14:41:54 -0700

    Quick release - patch

--- src/lib/middleware/express.ts
+++ src/lib/middleware/express.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { Logger } from '@srclaunch/logger';
 import { Exception } from '../../index';
 
-export const expressExceptionMiddleware = async (
+export const expressExceptionMiddleware = (
   err: Exception | Error,
   logger: Logger,
   req: Request,
